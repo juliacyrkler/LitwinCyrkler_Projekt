bcee256977d465f52dad8fe2a66dc23d707d51ad
juliacyrkler <julia.cyrkler@gmai.com>
2025-11-20 17:22:10 +0100

    Ekran powitalny z logowaniem (na razie bez laczenia z baza)

--- LitwinCyrkler_Projekt/Sklep.h
+++ LitwinCyrkler_Projekt/Sklep.h
@@ -4,15 +4,19 @@
 #include <string>
 #include <vector>
 #include "Produkt.h"
+#include "Uzytkownik.h"
 
 using namespace std;
 
 class Sklep {
 private:
+    Uzytkownik* uzytkownik;
     vector <Produkt> produkty;
 public:
     Sklep();
+	void ekranPowitalny();
 	bool zaloguj(string login, string haslo);
+	bool utworzKonto(string imie, string nazwisko, string login, string haslo);
 };
 
 #endif
